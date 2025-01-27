dfd46730421b1f3b5644eaffbc27ffe39d7aa5fb
GitHub <GitHub>
2025-01-27 18:58:03 +0900

    devise：カスタマイズ予定機能メモ追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     session: "users/session",
     registration: "users/registration"
   }
+  #採用したい機能　:Omniauthable
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
