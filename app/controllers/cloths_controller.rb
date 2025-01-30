3653a1638d536c36c3f1443aca7fde31a4ff5609
GitHub <GitHub>
2025-01-30 19:33:48 +0900

    improve:lint

--- app/controllers/cloths_controller.rb
+++ app/controllers/cloths_controller.rb
@@ -1,35 +1,33 @@
 class ClothsController < ApplicationController
-
   def index
     @cloths = Cloth.all
   end
-    
+
   def show
     @cloth = Cloth.find(params[:id])
   end
-    
+
   def new
     @cloth = Cloth.new
   end
-    
+
   def create
     @cloth = Cloth.new(cloth_params)
   end
-      
+
   def edit
     @cloth = Cloth.find(params[:id])
   end
-    
+
   def update
-    
   end
-    
+
   def destroy
   end
 
   private
 
   def cloth_params
-    params.reqire(:cloth).permit(:brand, :body, :purchase_date, :price)
+    params.require(:cloth).permit(:brand, :body, :purchase_date, :price)
   end
 end
