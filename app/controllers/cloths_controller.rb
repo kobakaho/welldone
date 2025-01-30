9dc9d27775bf17a3a3c10d63c9a6b54915f4a443
GitHub <GitHub>
2025-01-30 14:48:11 +0900

    add:show雛形

--- app/controllers/cloths_controller.rb
+++ app/controllers/cloths_controller.rb
@@ -5,20 +5,29 @@ class ClothsController < ApplicationController
   end
     
   def show
+    @cloth = Cloth.find(params[:id])
   end
     
   def new
+    @cloth = Cloth.new
   end
     
   def create
   end
       
   def edit
+    @cloth = Cloth.find(params[:id])
   end
     
   def update
   end
     
   def destroy
+  end
+
+  private
+
+  def cloth_params
+    params.reqire(:cloth).permit(:image_file, :brand, :body, :purchase_date, :price)
   end
 end
