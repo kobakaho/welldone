e7404a98edbff23b7ac3f2618d92937bd99af4ab
GitHub <GitHub>
2025-02-19 21:55:36 +0900

    add:取得したparent_idをcontrollrに送る

--- app/javascript/controllers/category_controller.js
+++ app/javascript/controllers/category_controller.js
@@ -3,17 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="category"
 export default class extends Controller {
   connect() {
-    console.log("category.js読み込みに成功しました")
+    console.log("category.js 読み込みに成功しました")
   }
+
   fetchChildren(event) {
     const parentId = event.target.value;
+
     if (parentId) {
       fetch(`/categories/get_children?parent_id=${parentId}`, {
-        headers: { Accept: "text/vnd.turbo-stream.html" }
+        headers: { 
+          Accept: "text/vnd.turbo-stream.html"
+        }
       })
       .then(response => response.text())
-      .then(html => Turbo.renderStreamMessage(html));
+      .then(html => {
+        document.getElementById("get_children").innerHTML = html;
+      })
+      .catch(error => console.error("Error:", error));
     }
   }
 }
-
