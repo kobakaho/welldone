524eb53d65a3720cd16908c668afd5b797d3d48a
GitHub <GitHub>
2025-02-21 17:28:29 +0900

    add:アカウント情報更新時にプロフィール画像更新を許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :configure_permitted_parameters, if: :devise_contoller?
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def after_sign_in_path_for(resources)
@@ -10,5 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(keys: [:profile_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:profile_image])
+  end
 end
