ac2a6cf6e7b81993ffb486edc7459596621b32d8
GitHub <GitHub>
2025-02-22 01:47:10 +0900

    fix:test carrierwave.rb修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,15 @@
-require "carrierwave/storage/abstract"
-require "carrierwave/storage/file"
-require "carrierwave/storage/fog"
-
-CarrierWave.configure do |config|
-    config.storage :fog
-    config.fog_provider = "fog/aws"
-    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: "ap-northeast-1",
-      path_style: true
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
     }
+
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.cache_storage = :fog
+  end
 end
