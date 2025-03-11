a54da163f75963f4c92ae48de479b54bb9af4a32
GitHub <GitHub>
2025-03-11 18:57:43 +0900

    cloth defaul_image追加

--- app/uploaders/cloth_image_uploader.rb
+++ app/uploaders/cloth_image_uploader.rb
@@ -18,12 +18,13 @@ class ClothImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
+  def default_url(*args)
+    "no_image_default"
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
