b3251908399bba84c210dc06de709caebc4d899a
GitHub <GitHub>
2025-03-13 19:17:28 +0900

    seasonをseed.rbに明示的に追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,9 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-#  本番環境に適応する分
-#  ["Spring","Summer","Autumn","Winter"].each do |season|
-#    Season.find_or_create_by!(name: season)
-#  end
+[ "Spring", "Summer", "Autumn", "Winter" ].each do |name|
+  Season.find_or_create_by!(name: name)
+end
 # "着るもの" => ["毛玉が取れない","汚れ・ほつれがある","着心地が悪い","1年以上着ていない","合わせるのが難しい","シワになりやすい","体型が変わって着れない","高かったからが理由で手放せない","着ていてもテンションが上がらない"],
 # "身に着けるもの" => ["デザイン・用途が似ている","使い勝手が悪い","1年以上使ってない","必要以上の量がある","高かったからが理由で手放せない","使ってもテンションが上がらない","「人に見られないからいいか」と使い続けている"]
 
