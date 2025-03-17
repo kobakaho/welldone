e0b39751f86a38f3fb2983bf0beae49ee8da85a3
GitHub <GitHub>
2025-03-17 23:28:25 +0900

    usernameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_28_163026) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_17_142204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_28_163026) do
     t.string "provider"
     t.string "uid"
     t.string "profile_image"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider"
