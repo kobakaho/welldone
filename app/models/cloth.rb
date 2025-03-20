4245be5f20dc9b9984c808aed60e912bad43e56f
GitHub <GitHub>
2025-03-21 00:43:08 +0900

    cloth.image_fileにバリデーションを追加

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -5,6 +5,7 @@ class Cloth < ApplicationRecord
   validates :price, allow_nil: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 }
   validates :season_ids, presence: true
   validates :category_ids, presence: true
+  validates :image_file, presence: true
 
   belongs_to :user
 
