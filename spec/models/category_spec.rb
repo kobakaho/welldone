765eb7dba0da7c3c0be132bccfea32751ed9809d
GitHub <GitHub>
2025-04-02 01:52:52 +0900

    バリデーションチェック（season,category）

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    context 'name' do
+      it '値がある場合成功' do
+        category = build(:category, name: 'トップス')
+        expect(category).to be_valid
+      end
+
+      it '値がない場合失敗' do
+        category = build(:category, name: " ")
+        expect(category).to be_invalid
+        expect(category.errors[:name]).not_to be_empty
+      end
+    end
+  end
 end
--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Season, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    context 'name' do
+      it '値がある場合成功' do
+        season = build(:season, name: 'Spring')
+        expect(season).to be_valid
+      end
+
+      it '値がない場合失敗' do
+        season = build(:season, name: " ")
+        expect(season).to be_invalid
+        expect(season.errors[:name]).not_to be_empty
+      end
+    end
+  end
 end
