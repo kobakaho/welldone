6cf740367efc38bd5ec58642b2d4dc23635f0693
GitHub <GitHub>
2025-04-08 23:42:58 +0900

    get_parents: categories#get_parentsを追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,15 @@
 class CategoriesController < ApplicationController
+  def get_parents
+    root_category = Category.find(params[:root_id])
+    @parents = root_category.children
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("get_parents", partial: "categories/get_parents", locals: { parents: @parents })
+      end
+    end
+  end
+
   def get_children
     parent_category = Category.find(params[:parent_id])
     @children = parent_category.children
