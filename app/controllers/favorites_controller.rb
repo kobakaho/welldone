d4932a28763895030f430d591fbb1ae7236b92c2
GitHub <GitHub>
2025-04-08 14:34:06 +0900

    favorite: #create,#destroyのajax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,11 @@
 class FavoritesController < ApplicationController
   def create
-    cloth = current_user.cloths.find(params[:cloth_id])
-    current_user.favorite(cloth)
-    redirect_to cloth_path(cloth), notice: "成功"
+    @cloth = current_user.cloths.find(params[:cloth_id])
+    current_user.favorite(@cloth)
   end
 
   def destroy
-    cloth = current_user.favorites.find(params[:id]).cloth
-    current_user.unfavorite(cloth)
-    redirect_to cloth_path, notice: "消した！"
+    @cloth = current_user.favorites.find(params[:id]).cloth
+    current_user.unfavorite(@cloth)
   end
 end
