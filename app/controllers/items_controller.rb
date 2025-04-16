794634596499ff972edd9d3c636233a48d124309
GitHub <GitHub>
2025-04-17 00:05:50 +0900

    item: default_itemとoriginal_itemを操作するためのコントローラー

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,29 @@
+class ItemsController < ApplicationController
+  def new
+    @checklist = current_user.checklists.find(params[:checklist_id])
+    @original_item = OriginalItem.new
+    @original_items = @checklist.original_items.order(created_at: :desc)
+    @default_items = DefaultItem.all
+  end
+
+
+  def create
+    @checklist = current_user.checklists.find(params[:checklist_id])
+    @original_item = @checklist.original_items.build(original_item_params) 
+
+    if @original_item.save
+      @checklist.original_items << @original_item
+    end
+  end
+
+  def destroy
+    @original_item = OriginalItem.find(params[:id])
+    @original_item.destroy!
+  end
+
+  private
+
+  def original_item_params
+    params.require(:original_item).permit(:name)
+  end
+end
