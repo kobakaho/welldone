2bfff71f71bd498a583c622f254dc1cf89445b24
GitHub <GitHub>
2025-04-19 16:59:42 +0900

    actionmailer: テスト用mailerテンプレート

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,7 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example"
-  
   def comeback_email
-    @user = user
+    @user_name = "test"
     @url = "http://localhost:3000/users/sign_in"
-    mail(to: @user.email, subject: "断捨離の時期が近づいてきました！！")
+    mail(to: "test@example.com", subject: "断捨離の時期が近づいてきました！！")
   end
 end
