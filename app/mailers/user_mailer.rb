401803804ad5e854a46bd4234b5d5b426c214a79
GitHub <GitHub>
2025-04-20 17:59:22 +0900

    lint

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def comeback_email(user)
     @user = user
     @user_name = @user.username
-    
+
     if development
       @url = "http://localhost:3000/users/sign_in"
     else production
