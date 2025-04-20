81944201c02e0e60d6a673ca287d6acc36cb9c69
GitHub <GitHub>
2025-04-20 17:54:07 +0900

    action_mailer: 本番環境用サーバー設定

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,13 @@ class UserMailer < ApplicationMailer
   def comeback_email(user)
     @user = user
     @user_name = @user.username
-    @url = "http://localhost:3000/users/sign_in"
+    
+    if development
+      @url = "http://localhost:3000/users/sign_in"
+    else production
+      @url = "http://welldoneshari.com/users/sign_in"
+    end
+
     mail(
       to: @user.email,
       subject: "断捨離の時期が近づいてきました！！"
