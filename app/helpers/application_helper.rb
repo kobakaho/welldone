c79f1fbb7329490fdd0420ddde62de49b248e152
GitHub <GitHub>
2025-04-26 18:08:14 +0900

    meta_tag: 本番環境用URLに修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,19 +6,21 @@ module ApplicationHelper
       reverse: true,
       charset: "utf-8",
       description: "クローゼットを管理し、断捨離を促すサービス",
+      canonical: "welldoneshari.com",
+      separator: "|",
       og: {
-        site_name: "well断",
-        title: "well断",
-        description: "クローゼットを管理し、断捨離を促すサービス",
+        site_name: :site,
+        title: :title,
+        description: :description,
         type: "website",
-        url: request.original_url,
-        image: image_url("top_image.jpg"),#なんの画像やねん
+        url: "welldoneshari.com",
+        image: image_url("top_image.png"),
         local: "ja-JP"
       },
       twitter: {
-        card: "summary_large_image",
-        # site: 自分のツイッターアカウント
-        image: image_url("top_image.jpg")#なんの画像やねん
+        card: "summary",
+        site: "@",
+        image: image_url("top_image.png")
       }
     }
   end
