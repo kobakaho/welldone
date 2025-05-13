469db30c56ff9c456da7bc32433977290c39ff64
GitHub <GitHub>
2025-05-13 23:42:50 +0900

    ogp: image_urlを指定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,22 +5,22 @@ module ApplicationHelper
       title: "well断",
       reverse: true,
       charset: "utf-8",
-      description: "クローゼットを管理し、断捨離を促すサービス",
-      canonical: "welldoneshari.com",
+      description: "クローゼットを管理し、断捨離をサポートするサービス",
+      canonical: "http://welldoneshari.com",
       separator: "|",
       og: {
         site_name: :site,
         title: :title,
         description: :description,
         type: "website",
-        url: "welldoneshari.com",
-        image: image_url("top_image.png"),
+        url: "http://welldoneshari.com",
+        image: image_url("ogp.png"),
         local: "ja-JP"
       },
       twitter: {
-        card: "summary",
+        card: "summary_large_image",
         site: "@",
-        image: image_url("top_image.png")
+        image: image_url("ogp.png")    
       }
     }
   end
