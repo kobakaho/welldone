634ab5131694a18a4eba3900b7dfd8b81783f379
GitHub <GitHub>
2025-05-13 23:37:52 +0900

    アプリ全体にbefore_action :authenticate_user!を設定したことによる修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :authenticate_user!
+  
   private
 
   def after_sign_in_path_for(resources)
